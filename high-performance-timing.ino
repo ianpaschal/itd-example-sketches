011efcf383938f468a94e8ccfab4e7e458c1b58b
Ian Paschal <ian.paschal@gmail.com>
2017-06-15 12:07:42 +0200

    Base sketches
    They still need content though

---
+++ high-performance-timing.ino
@@ -0,0 +1,16 @@
+/*
+	ITD Example Sketches | Handle Buttons
+	-
+	Authors: Ian Paschal (ianpaschal)
+	License: Apache License 2.0
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
